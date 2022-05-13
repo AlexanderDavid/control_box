ec80bf0a46cade7c2a13aa8c578ad2e34f8672d5
Alex Day <alexday135@gmail.com>
2022-05-13 17:47:45 -0400

    Add rotary switch

--- scripts/control_box_node.py
+++ scripts/control_box_node.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 from typing import List
 
 import rospy
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from std_msgs.msg import UInt8, UInt8MultiArray
-from control_box.msg import Button
+import RPi.GPIO as GPIO
+from control_box.msg import Button as ButtonMsg
+from control_box.msg import RotarySwitch as RotarySwitchMsg
 
 class RotarySwitch:
     def __init__(self, pins: List[int], name: str):
@@ -14,28 +14,30 @@ class RotarySwitch:
         for pin in pins:
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self.name = name
-        self.pub = rospy.Publisher(f"/controller_box/{self.name}", UInt8MultiArray, queue_size=10)
+        self.pub = rospy.Publisher(f"/controller_box/{self.name}", RotarySwitchMsg, queue_size=10)
 
     def spin(self):
         states = []
         for pin in self.pins:
             states.append(GPIO.input(pin) == GPIO.HIGH)
 
-        msg = UInt8MultiArray()
-        msg.data = states
+        msg = RotarySwitchMsg()
+        msg.header.stamp = rospy.Time.now()
+        msg.name = self.name
+        msg.pins = self.pins
+        msg.states = states
         self.pub.publish(msg)
 
-class Switch:
+class Button:
     def __init__(self, pin: int, name: str, no: bool=True):
         self.pin = pin
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self.name = name
-        self.pub = rospy.Publisher(f"/controller_box/{self.name}", Button, queue_size=10)
+        self.pub = rospy.Publisher(f"/controller_box/{self.name}", ButtonMsg, queue_size=10)
         self.no = no
 
     def spin(self):
-
-        msg = Button()
+        msg = ButtonMsg()
         msg.header.stamp = rospy.Time.now()
         msg.pin = self.pin
         msg.name = self.name
@@ -56,8 +58,8 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
     inputs = [
-        Switch(26, "estop", False),
-        Switch(19, "switch", False),
+        Button(26, "estop", False),
+        Button(19, "switch", False),
         RotarySwitch([21, 20, 16, 18, 25, 24], "cmd_vel_muxer")
     ]
 
